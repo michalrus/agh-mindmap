49bd15a810787d212c6123d78061bad93083c33f
Michał Rus <m@michalrus.com>
2013-10-15 16:02:59 +0200

    Fix & use MainActivity

--- android/src/main/scala/edu/agh/mindmap/activity/MainActivity.scala
+++ android/src/main/scala/edu/agh/mindmap/activity/MainActivity.scala
@@ -5,9 +5,13 @@ import scala.collection.mutable
 import com.actionbarsherlock.app.SherlockFragmentActivity
 import android.os.Bundle
 import edu.agh.mindmap.R
-import edu.agh.mindmap.helper.ScalaActivity
 import android.widget.TabHost
 import android.support.v4.app.{Fragment, FragmentActivity}
+import android.content.Context
+import android.view.View
+import edu.agh.mindmap.fragment.MapListFragment
+import scala.reflect.ClassTag
+import com.michalrus.helper.ScalaActivity
 
 class MainActivity extends SherlockFragmentActivity with ScalaActivity {
 
@@ -18,14 +22,23 @@ class MainActivity extends SherlockFragmentActivity with ScalaActivity {
     super.onCreate(bundle)
     setContentView(R.layout.main)
     tabHost.setup()
+
+    tabManager.addTab[MapListFragment]("all", "All maps")
+    tabManager.addTab[MapListFragment]("all2", "All maps 2")
+    tabManager.addTab[MapListFragment]("all3", "All maps 3")
+    tabManager.addTab[MapListFragment]("all4", "All maps 4")
+    tabManager.addTab[MapListFragment]("all5", "All maps 5")
+    tabManager.addTab[MapListFragment]("all6", "All maps 6")
   }
 
   class TabManager(val activity: FragmentActivity, tabHost: TabHost, containerId: Int) extends TabHost.OnTabChangeListener {
 
+    tabHost.setOnTabChangedListener(this)
+
     val creators = new mutable.HashMap[String, () => Fragment]
     val fragments = new mutable.HashMap[String, Fragment]
     var lastTabTag: Option[String] = None
-/*
+
     class DummyTabFactory(val context: Context) extends TabHost.TabContentFactory {
       override def createTabContent(tag: String) = {
         val v = new View(context)
@@ -34,14 +47,31 @@ class MainActivity extends SherlockFragmentActivity with ScalaActivity {
         v
       }
     }
-*/
-    def addTab(tag: String, label: String, creator: () => Fragment) {
-      val tabSpec = tabHost.newTabSpec(tag).setIndicator(label) //.setContent(new DummyTabFactory(activity))
-      // FIXME: does something go here really?
+
+    def addTab[F](tag: String, label: String, args: Bundle = null)(implicit classTag: ClassTag[F]) {
+      val tabSpec = tabHost.newTabSpec(tag).setIndicator(label).setContent(new DummyTabFactory(activity))
+      creators += tag -> (() => Fragment.instantiate(activity, classTag.runtimeClass.getName, args))
+
+      // Check to see if we already have a fragment for this tab, probably
+      // from a previously saved state.  If so, deactivate it, because our
+      // initial state is that a tab isn't shown. (On orientation change.)
+      Option(activity.getSupportFragmentManager.findFragmentByTag(tag)) match {
+        case Some(f) => {
+          if (!f.isDetached) {
+            val ft = activity.getSupportFragmentManager.beginTransaction
+            ft.detach(f)
+            ft.commit()
+          }
+          fragments += tag -> f
+        }
+        case _ =>
+      }
+
       tabHost.addTab(tabSpec)
     }
 
     override def onTabChanged(tag: String) {
+      log("onTabChanged: " + tag)
       if (lastTabTag != Some(tag)) {
         val ft = activity.getSupportFragmentManager.beginTransaction
         lastTabTag match {
@@ -51,6 +81,23 @@ class MainActivity extends SherlockFragmentActivity with ScalaActivity {
           }
           case _ =>
         }
+        (creators.get(tag), fragments.get(tag)) match {
+          case (Some(creator), None) => {
+            log("; creating fragment")
+            val f = creator()
+            fragments += tag -> f
+            ft.add(containerId, f, tag)
+          }
+          case (_, Some(f)) => {
+            log("; attaching fragment")
+            ft.attach(f)
+          }
+          case _ =>
+            log("; unknown case")
+        }
+        lastTabTag = Some(tag)
+        ft.commit()
+        activity.getSupportFragmentManager.executePendingTransactions()
       }
     }
 
