4d6d6a82ce0dcef95ecc35fc21b5428d7a634627
Michał Rus <m@michalrus.com>
2013-10-16 02:34:52 +0200

    Create a method to run something later on UI thread

--- android/src/main/scala/com/michalrus/helper/ScalaActivity.scala
+++ android/src/main/scala/com/michalrus/helper/ScalaActivity.scala
@@ -1,9 +1,17 @@
 package com.michalrus.helper
 
 import android.app.Activity
+import scala.concurrent.{ExecutionContext, future}
+import ExecutionContext.Implicits.global
 
 trait ScalaActivity extends Activity with ViewHelper {
 
   def find[T](id: Int) = findViewById(id).asInstanceOf[T]
+
+  def laterOnUiThread(r: Runnable) {
+    future {
+      runOnUiThread(r)
+    }
+  }
 
 }
