f9b96c661c03d5c4d33a144d8ce79dd4f968adec
katszwn <kat.szwn@gmail.com>
2013-10-26 18:46:33 +0200

    Sketch custom HorizontalScrollView with touch event propagation

---
+++ android/src/main/scala/edu/agh/mindmap/component/HorizontalScrollViewWithPropagation.java
@@ -0,0 +1,23 @@
+package edu.agh.mindmap.component;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+public class HorizontalScrollViewWithPropagation extends HorizontalScrollView {
+
+    public HorizontalScrollViewWithPropagation(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public HorizontalScrollViewWithPropagation(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HorizontalScrollViewWithPropagation(Context context) {
+        super(context);
+    }
+
+    // FIXME!
+
+}
