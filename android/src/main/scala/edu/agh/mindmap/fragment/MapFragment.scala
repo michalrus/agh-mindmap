4e6c13d0c95e760bc6d0ed7fae91a29891ae049f
katszwn <kat.szwn@gmail.com>
2013-11-13 16:48:32 +0100

    Prepare to put stuff on paper

--- android/src/main/scala/edu/agh/mindmap/fragment/MapFragment.scala
+++ android/src/main/scala/edu/agh/mindmap/fragment/MapFragment.scala
@@ -2,18 +2,22 @@ package edu.agh.mindmap.fragment
 
 import com.michalrus.helper.ScalaFragment
 import com.actionbarsherlock.app.SherlockFragment
-import android.view.{ViewGroup, LayoutInflater}
+import android.view.{View, ViewGroup, LayoutInflater}
 import android.os.Bundle
 import edu.agh.mindmap.R
 import java.util.UUID
-import edu.agh.mindmap.model.MindMap
+import edu.agh.mindmap.model.{MindNode, MindMap}
 import edu.agh.mindmap.component.HorizontalScrollViewWithPropagation
-import android.widget.ScrollView
+import android.widget.{RelativeLayout, ScrollView}
 
 class MapFragment extends SherlockFragment with ScalaFragment {
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, bundle: Bundle) = {
     val view = inflater.inflate(R.layout.map, container, false)
+
+    val hScroll = view.find[HorizontalScrollViewWithPropagation](R.id.hscroll)
+    val vScroll = view.find[ScrollView](R.id.vscroll)
+    hScroll.inner = vScroll
 
     val uuid = try {
       UUID.fromString(bundle getString "uuid")
@@ -21,15 +25,18 @@ class MapFragment extends SherlockFragment with ScalaFragment {
       case _: Exception => new UUID(0, 0)
     }
 
-    val map = MindMap findByUuid uuid
-
-    val hScroll = view.find[HorizontalScrollViewWithPropagation](R.id.hscroll)
-    val vScroll = view.find[ScrollView](R.id.vscroll)
-    hScroll.inner = vScroll
-
-    // FIXME
+    MindMap findByUuid uuid match {
+      case Some(m) => paintMap(m)
+      case _ =>
+    }
 
     view
+  }
+
+  def paintMap(map: MindMap)(implicit view: View) {
+    val paper = view.find[RelativeLayout](R.id.paper)
+
+    // FIXME
   }
 
 }
