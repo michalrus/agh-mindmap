3c650ef316b613dba44628dc8b9a39cf449d0145
Michał Rus <m@michalrus.com>
2013-12-22 03:10:41 +0000

    Add some safety margin around a node-connecting line

--- android/src/main/scala/edu/agh/mindmap/component/ArrowView.scala
+++ android/src/main/scala/edu/agh/mindmap/component/ArrowView.scala
@@ -4,12 +4,19 @@ import android.view.View
 import android.content.Context
 import android.graphics.{Paint, Canvas, Color}
 
-case class Arrow(x0: Int, y0: Int, x1: Int, y1: Int) {
+object Arrow {
+  // how much should the bounding box be bigger than the (x0,y0,x1,y1) rectangle?
+  val SafetyMargin = 10 // [px]
+  val DisplayBoundingBox = false
+}
 
-  val boundingX: Int = x0 min x1
-  val boundingY: Int = y0 min y1
-  val boundingW: Int = (x0 - x1).abs
-  val boundingH: Int = (y0 - y1).abs
+case class Arrow(x0: Int, y0: Int, x1: Int, y1: Int) {
+  import Arrow.{SafetyMargin => M}
+
+  val boundingX = -M + (x0 min x1)
+  val boundingY = -M + (y0 min y1)
+  val boundingW = 2 * M + (x0 - x1).abs
+  val boundingH = 2 * M + (y0 - y1).abs
 
   val innerX0 = x0 - boundingX
   val innerY0 = y0 - boundingY
@@ -22,7 +29,7 @@ class ArrowView(context: Context, var arrow: Arrow)
   extends View(context) {
 
   setClickable(false)
-  setBackgroundColor(Color.TRANSPARENT)
+  setBackgroundColor(if (Arrow.DisplayBoundingBox) Color.argb(0x11, 128, 0, 0) else Color.TRANSPARENT)
 
   override def onDraw(canvas: Canvas) {
     super.onDraw(canvas)
