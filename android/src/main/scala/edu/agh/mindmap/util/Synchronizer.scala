b6306a3eb0f92e43b97a24b589a845a595a5c4c9
Michał Rus <m@michalrus.com>
2014-01-03 12:38:37 +0000

    Add all JSON messages to Synchronizer.scala

--- android/src/main/scala/edu/agh/mindmap/util/Synchronizer.scala
+++ android/src/main/scala/edu/agh/mindmap/util/Synchronizer.scala
@@ -22,6 +22,8 @@ import ExecutionContext.Implicits.global
 import java.util.concurrent.atomic.AtomicBoolean
 import com.michalrus.helper.MiscHelper.log
 import scala.util.Success
+import java.util.UUID
+import spray.json._
 
 object Synchronizer {
   private val pollShouldRun = new AtomicBoolean(false)
@@ -83,5 +85,52 @@ object Synchronizer {
 
     log(s"POLL:    ... done")
   }
+
+}
+
+object Js {
+
+  trait CustomJsonFormats {
+
+    implicit object UuidFormat extends JsonFormat[UUID] {
+      def write(obj: UUID): JsValue = JsString(obj.toString)
+
+      def read(json: JsValue): UUID = json match {
+        case JsString(x) => try { UUID fromString x }
+        catch { case e: Throwable => deserializationError(s"UUID could not be parsed from `$x'", e) }
+        case x => deserializationError(s"Expected UUID as JsString, but got $x")
+      }
+    }
+
+  }
+
+  object MindNode extends DefaultJsonProtocol with CustomJsonFormats {
+    implicit val format = jsonFormat6(apply)
+  }
+
+  case class MindNode(uuid: UUID,
+                      parent: Option[UUID],
+                      ordering: Double,
+                      content: Option[String],
+                      hasConflict: Boolean,
+                      cloudTime: Long)
+
+  object PollResponse extends DefaultJsonProtocol {
+    implicit val format = jsonFormat1(apply)
+  }
+
+  case class PollResponse(nodes: List[MindNode])
+
+  object UpdateRequest extends DefaultJsonProtocol with CustomJsonFormats {
+    implicit val format = jsonFormat3(apply)
+  }
+
+  case class UpdateRequest(mindMap: UUID, lastServerTime: Long, nodes: List[MindNode])
+
+  object UpdateResponse extends DefaultJsonProtocol with CustomJsonFormats {
+    implicit val format = jsonFormat1(apply)
+  }
+
+  case class UpdateResponse(unknownParents: List[UUID])
 
 }
